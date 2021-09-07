cabb3c260c85e553af7670be92cee7257034be99
Boomika1506 <82639613+Boomika1506@users.noreply.github.com>
2021-09-07 23:12:06 +0530

    done

---
+++ 3_Implementation/src/division.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int division(int n1, int n2)
+{
+    
+    return (n1/n2);
+}
---
+++ 3_Implementation/src/power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h> // math support some mathemetics inbuilt function
+int power(int a, int b)
+{
+    int i=1;
+     int p; //declaring p 
+     while(i!=b){
+          p *=a; //calculating power (a,b) and storing in 'p'
+          return p; //returning power value to main funtion 
+  
+
+   
+     }
+}
